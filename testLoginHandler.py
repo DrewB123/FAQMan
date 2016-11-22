ba4a677d419195cd5a8ccfb168987157c851a5e5
Heathur <heather.m.noble@gmail.com>
2016-11-22 02:11:42 -0600

    Add files via upload

--- testLoginHandler.py
+++ testLoginHandler.py
@@ -11,8 +11,6 @@ import webapp2
 from google.appengine.ext import ndb
 from google.appengine.ext import testbed
 
-#app = TestApp(main.app)
-
 class LoginHandlerTests(unittest.TestCase):
 	# Set up the environment.
 	def setUp(self):
@@ -30,92 +28,34 @@ class LoginHandlerTests(unittest.TestCase):
 	def tearDown(self): 
 		self.testbed.deactivate()
 	
-	# Mocked test
-	@patch.object(jinja2.Environment, 'get_template')
-	# The patched function is called 'mock' in this context.
-	def testMainHandlerGetMocked(self, mock): 
-		# Arrange: Make a fake web request that will be routed to MainHandler's get().
-		request = webapp2.Request.blank('/home')
-		# Act: Give the request to the app. 
-		response = request.get_response(main.app)
-		# Assert
-		mock.assert_called_with('home-page.html')
-		# steve holt
-	
-	# Mocked test: verify that the post() redirect works when a duplicate user error happens.
-	@patch.object(webapp2.RequestHandler, 'redirect')
-	def testMainHandlerPostEmailError(self, mock): 
-		# Arrange: If there's not exactly one user, an error occurs. 
-		# POST is a dictionary; it adds this data to a request object...or something. I think this is unnecessary.
-		test_request = webapp2.Request.blank('/home', POST={"user_email": "fake_user@email.com", "pass_word" : "FAKEPASSWORD"})
-		test_request.method = 'POST'
-		# I have no idea why this line is necessary. 
-		mock.return_value = None
-		# Act: Give the request to the app. 
-		response = test_request.get_response(main.app)
-
-		# Assert: Inspect the response
-		mock.assert_called_with('/home')
-		self.assertEqual(main.error, "Invalid email!")
-	
-	# Mocked test: ensure that the application does not allow login with an incorrect password.
-	@patch.object(webapp2.RequestHandler, 'redirect')
-	def testMainHandlerPostPWError(self, mock): 
-		# Arrange: Put a User in the datastore stub; POST the wrong password.
-		test_User = User()
-		test_User.email = "fake_user@email.com"
-		test_User.password = "PASSWORD"
-		test_User.put()
-		test_request = webapp2.Request.blank('/home', POST={"user_email" : "fake_user@email.com", "pass_word" : "WRONGPASSWORD"})
-		test_request.method = 'POST'
-		mock.return_value = None
-
-		# Act: Give the request to the app.
-		response = test_request.get_response(main.app)
-
-		# Assert: Inspect the response. 
-		mock.assert_called_with('/home')
-		self.assertEqual(main.error, "Incorrect password!")
-	
-	# Mocked test: ensure that login occurs when the correct user name and password are entered.
-	@patch.object(webapp2.RequestHandler, 'redirect')
-	def testMainHandlerPostLogin(self, mock): 
-		# Arrange: Put a User in the datastore stub; POST the correct password. 
-		test_User = User()
-		test_User.email = "fake_user@email.com"
-		test_User.password = "PASSWORD"
-		test_User.put()
-		test_request = webapp2.Request.blank('/home', POST={"user_email" : "fake_user@email.com", "pass_word" : "PASSWORD"})
-		test_request.method = 'POST'
-		mock.return_value = None
-
-		# Act: Give the request to the app.
-		response = test_request.get_response(main.app)
-
-		# Assert: Inspect the response. 
-		# The "error" should be blank. 
-		mock.assert_called_with('/login')
-		self.assertEqual(main.error, "")
-
-	@patch.object(webapp2.RequestHandler, 'redirect')
-	@patch.object(webapp2.Response, 'set_cookie')
-	def testMainHandlerCookie(self, mock_set_cookie, mock_redir):
+	# Mocked test: ensure that cookies function on login for Student user.
+	@patch.object(jinja2.Environment, 'get_template') 
+	def testLoginHandlerCookieStudent(self, mock):
 		user = User()
 		user.Fname = "Matt"
 		user.Lname = "K"
 		user.email = "mattk@uwm.edu"
 		user.password = "KMatt"
+		user.isInstructor = False
+		main.u = user
 		user.put()
-		request = webapp2.Request.blank('/home', POST={"user_email": "mattk@uwm.edu", "pass_word" : "KMatt"})
-		request.method = 'POST'
-		mock_redir.return_value = None
-		
+		request = webapp2.Request.blank('/login')
+		request.cookies['uname'] = "mattk@uwm.edu"
 		response = request.get_response(main.app)
-		
-		mock_set_cookie.assert_called_with('uname', user.email, path='/')
-		self.assertEqual(main.error, "")
-		mock_redir.assert_called_with('/login')
+		mock.assert_called_with('student_landing.html')
 
-# 'main' method. 
-if __name__ == '__main__':
-	unittest.main()
+	# Mocked test: ensure that cookies function on login for Faculty
+	@patch.object(jinja2.Environment, 'get_template') 
+	def testLoginHandlerCookieFaculty(self, mock):
+		user = User()
+		user.Fname = "Matt"
+		user.Lname = "K"
+		user.email = "mattk@uwm.edu"
+		user.password = "KMatt"
+		user.isInstructor = True
+		main.u = user
+		user.put()
+		request = webapp2.Request.blank('/login')
+		request.cookies['uname'] = "mattk@uwm.edu"
+		response = request.get_response(main.app)
+		mock.assert_called_with('Faculty_landing.html')
